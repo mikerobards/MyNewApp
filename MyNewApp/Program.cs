8d8534b87752d8685be1dc0165842d5648df0c46
Michael Robards <mikerobards@icloud.com>
2021-08-20 16:29:45 -0400

    add class utility

--- MyNewApp/Program.cs
+++ MyNewApp/Program.cs
@@ -6,7 +6,19 @@ namespace MyNewApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string value = "Microsoft Learn";
+            string reversedValue = Utility.Reverse(value);
+            Console.WriteLine($"Secret message: {reversedValue}");
+        }
+    }
+
+    class Utility
+    {
+        public static string Reverse(string message)
+        {
+            char[] letters = message.ToCharArray();
+            Array.Reverse(letters);
+            return new string(letters);
         }
     }
 }
